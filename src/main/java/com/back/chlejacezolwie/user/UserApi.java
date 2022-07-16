779999c4d17bb516e82895c4b980f69b90526f13
Krystian Blajek <krystian.51@interia.pl>
2022-07-16 18:09:19 +0200

    Several comments removed

--- src/main/java/com/back/chlejacezolwie/user/UserApi.java
+++ src/main/java/com/back/chlejacezolwie/user/UserApi.java
@@ -2,7 +2,10 @@ package com.back.chlejacezolwie.user;
 
 import java.util.Optional;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,17 +22,17 @@ public class UserApi {
 	@Autowired
 	UserRepository userRepository;
 
-	//https://spring.io/guides/gs/rest-service-cors/
 	//https://www.innoq.com/en/blog/cookie-based-spring-security-session/
 	
-	//@CrossOrigin(origins = "https://chlejace-zolwie-front.herokuapp.com/")
-	@PostMapping(path = "/join_game")
+	@PostMapping(value = "/join_game")
 	public void joinGame(@RequestBody RoomParameters roomParam) {
 		
 		//for testing
 		System.out.println(roomParam.getX());
 		System.out.println(roomParam.getY());
 		System.out.println(roomParam.getZ());
+		
+		//System.out.println(session.getId());
 		/*
 		if(userRepository.findBySession(sessionId).isEmpty()) {
 			
