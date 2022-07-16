c0a206bb1cebd9f7c1b8db6ffd4f07f77a50167f
Krystian Blajek <krystian.51@interia.pl>
2022-07-16 19:47:05 +0200

    added cors origin for localhost

--- src/main/java/com/back/chlejacezolwie/security/WebConfig.java
+++ src/main/java/com/back/chlejacezolwie/security/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("https://chlejace-zolwie-front.herokuapp.com/")
+            .allowedOrigins("https://localhost")
             .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true)
