550781fb8e9d3b53791a0072a1f79f23d69a6c0b
krystian-b <krystian.51@interia.pl>
2022-08-28 18:49:06 +0200

    session config added

--- src/main/java/com/back/chlejacezolwie/security/ApplicationSecurityConfig.java
+++ src/main/java/com/back/chlejacezolwie/security/ApplicationSecurityConfig.java
@@ -14,7 +14,7 @@ public class ApplicationSecurityConfig{
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 	    http.sessionManagement(session ->
-	    	session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+	    	session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
 	    		);
 	    http.sessionManagement(session ->
 	    	session.maximumSessions(1));
